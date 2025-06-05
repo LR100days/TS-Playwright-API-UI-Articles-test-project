998fcc37b5e922ea5523a77ea3be7b33e122dac9
Lidiia R <lidiia12rusakova@gmail.com>
2025-06-05 21:04:24 +0200

    Add automatic data  generation  in negativeTestsForLoginTestData file

--- tests/API-tests/negativeTests.spec.ts
+++ tests/API-tests/negativeTests.spec.ts
@@ -1,23 +1,23 @@
 import { test } from '../../utils/fixtures';
 import { expect } from '../../utils/custom-expect'
 import { lengthValidationForUsername, lengthValidationForPassword } from '../../test_data/negativeTestsForLoginTestData'
-import { faker } from '@faker-js/faker'
 
 lengthValidationForUsername.forEach(({username, usernameErrorMessage}) => {
-    test(`Error message validation for username ${username}`, async({api}) => {
+    test(`Error message validation for username length ${username}`, async({api}) => {
+        const actualUserName = typeof username === 'function' ? username() : username;
         const newUserResponse = await api
             .path('/users')
             . body({
                 "user":{
                     "email": "d",
                     "password": "d",
-                    "username": username
+                    "username": actualUserName
                 }
             })
             .clearAuth()
             .postRequest(422)
 
-        if(username.length == 3 || username.length == 20){
+        if(actualUserName.length == 3 || actualUserName.length == 20){
             expect(newUserResponse.errors).not.toHaveProperty('username')
         } else{
             expect(newUserResponse.errors.username[0]).shouldEqual(usernameErrorMessage)
@@ -28,20 +28,21 @@ lengthValidationForUsername.forEach(({username, usernameErrorMessage}) => {
 })
 
 lengthValidationForPassword.forEach(({password, passwordErrorMessage}) => {
-    test(`Error message validation for password ${password}`, async({api}) => {
+    test(`Error message validation for password length ${password}`, async({api}) => {
+        const actualPassword = typeof password === 'function' ? password() : password;
         const newUserResponse = await api
             .path('/users')
             . body({
                 "user":{
                     "email": "d",
-                    "password": password,
+                    "password": actualPassword,
                     "username": "d"
                 }
             })
             .clearAuth()
             .postRequest(422)
 
-        if(password.length == 8 || password.length == 20){
+        if(actualPassword.length == 8 || actualPassword.length == 20){
             expect(newUserResponse.errors).not.toHaveProperty('password')
         } else{
             expect(newUserResponse.errors.password[0]).shouldEqual(passwordErrorMessage)
@@ -51,34 +52,4 @@ lengthValidationForPassword.forEach(({password, passwordErrorMessage}) => {
 
 });
 
-[
-    { username: '', usernameErrorMessage: "can't be blank"},
-    { username: faker.lorem.sentence(5), usernameErrorMessage: 'is too short (minimum is 3 characters)'},
-    { username: 'ddr', usernameErrorMessage: ''},
-    { username: 'dddddddddddddddddddu', usernameErrorMessage: ''},
-    { username: 'ddddddddddddddddddddg', usernameErrorMessage: 'is too long (maximum is 20 characters)'}
-
-].forEach(({username, usernameErrorMessage}) => {
-    test(`Error message validation for faker username ${username}`, async({api}) => {
-        const newUserResponse = await api
-            .path('/users')
-            . body({
-                "user":{
-                    "email": "d",
-                    "password": "d",
-                    "username": username
-                }
-            })
-            .clearAuth()
-            .postRequest(422)
-
-        if(username.length == 3 || username.length == 20){
-            expect(newUserResponse.errors).not.toHaveProperty('username')
-        } else{
-            expect(newUserResponse.errors.username[0]).shouldEqual(usernameErrorMessage)
-        }
-        
-    })
-
-})
 
