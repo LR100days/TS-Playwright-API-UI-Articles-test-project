c591c4ed97ea94dc5148f6b6440858579e0a62a7
Lidiia R <lidiia12rusakova@gmail.com>
2025-06-13 13:35:05 +0200

    Add negative tests for Log In page

--- page_objects/loginPage.ts
+++ page_objects/loginPage.ts
@@ -1,19 +1,76 @@
-import { Page, expect } from '@playwright/test';
+import  { Page, expect } from '@playwright/test';
 import { config } from '../api-test.config';
-
 
 
 export class LoginPage {
     readonly page: Page
     constructor(page: Page) {
-        this.page = page
+        this.page = page    
     }
 
-    async loginWithValidCredentials(userLogin: string, userPassword: string){
-        await this.page.goto(`${config.baseURL}/login`)
-        await this.page.getByPlaceholder('Email').fill(userLogin)
-        await this.page.getByPlaceholder('Password').fill(userPassword)
+    async loginWithValidCredentials(){
+        await this.page.getByPlaceholder('Email').fill(config.userEmail)
+        await this.page.getByPlaceholder('Password').fill(config.userPassword)
         await this.page.getByRole('button', { name: 'Sign in' }).click();
     }
+
+    async enterConfiguredValidEmail(){
+        await this.page.getByPlaceholder('Email').fill(config.userEmail)
+    }
+
+    async enterConfiguredValidPassword(){
+        await this.page.getByPlaceholder('Password').fill(config.userPassword)
+    }
+
+    async clickSignInButton(){
+        await this.page.getByRole('button', { name: 'Sign in' }).click();
+    }
+
+    async enterInvalidEmail(){
+        await this.page.getByPlaceholder('Email').fill('a')
+    }
+
+    async enterInvalidPassword(){
+        await this.page.getByPlaceholder('Password').fill('1')
+    }
+
+    async verifyErrorMessageIs(errorMessage: string){
+        await expect(this.page.locator('.error-messages li')).toHaveText(errorMessage)
+    }
+
+    async verifySignInButtonIsDisabled(buttonState: boolean){
+        if(buttonState==true){
+            await expect(this.page.getByRole('button', { name: 'Sign in' })).toBeDisabled()
+        } else{
+            await expect(this.page.getByRole('button', { name: 'Sign in' })).toBeEnabled()}
+        
+    }
+
+    async verifySignInFormIsShown(){
+        expect(this.page.locator('h1')).toHaveText('Sign in')
+    }
+
+    async clearField(fieldName: string){
+        await this.page.getByPlaceholder(fieldName).clear()
+    }
+
+    async enterOnlySpacesForField(fieldName: string){
+        await this.page.getByPlaceholder(fieldName).fill('        ')
+    }
+
+    async enterPasswordForUser2(){
+        await this.page.getByPlaceholder('Password').fill(config.anotherUser2Password)
+    }
+
+    async enterEmailForUser2(){
+        await this.page.getByPlaceholder('Email').fill(config.anotherUser2Email)
+    }
+
+    
+
+    
+
+
+    
 
 }
---
+++ tests/UI-tests/negativeTestsLogInUI.spec.ts
@@ -0,0 +1,109 @@
+import { test } from '../../utils/fixtures';
+import { LoginPage } from '../../page_objects/loginPage';
+import { config } from '../../api-test.config';
+
+test.beforeEach( async({page}) => {
+  await page.goto(`${config.baseURL}/login`)
+})
+
+test.describe("Verify error messages on Log In page", async () => {
+    test('Error message validation for invalid password and email', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterInvalidEmail()
+        await onLoginPage.enterInvalidPassword()
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs('email or password is invalid')
+        await onLoginPage.verifySignInFormIsShown()
+    })
+
+    test('Error message validation for invalid password and valid email', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.enterInvalidPassword()
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs('email or password is invalid')
+        await onLoginPage.verifySignInFormIsShown()
+    })
+
+    test('Error message validation for valid password and invalid email', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterInvalidEmail()
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs('email or password is invalid')
+        await onLoginPage.verifySignInFormIsShown()
+    })
+
+    test('Error message validation for email and password fields when only spaces are entered', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterOnlySpacesForField('Email')
+        await onLoginPage.enterOnlySpacesForField('Password')
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs("email can't be blank")
+        await onLoginPage.verifySignInFormIsShown()
+    })
+
+    test('Error message validation when password for different user is entered', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.enterPasswordForUser2()
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs("email or password is invalid")
+        await onLoginPage.verifySignInFormIsShown()
+
+    })
+
+    test('Error message validation when email for different user is entered', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterEmailForUser2()
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.clickSignInButton()
+        await onLoginPage.verifyErrorMessageIs("email or password is invalid")
+        await onLoginPage.verifySignInFormIsShown()
+
+    })
+
+})
+
+
+test.describe("Verify Sign In button state on Log In page", async () => {
+    test('Verify Sign In button state  when email and password fields are empty', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.verifySignInButtonIsDisabled(true) 
+    })
+    
+    test('Verify Sign In button state  when valid email is entered and password field is empty', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.verifySignInButtonIsDisabled(true)
+    })
+
+    test('Verify Sign In button state  when email field is empty and valid password is entered', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.verifySignInButtonIsDisabled(true)
+    })
+
+    test('Verify Sign In button state  when valid email and password are entered', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.verifySignInButtonIsDisabled(false)
+    })
+
+    test('Verify Sign In button state when data from required fields is removed', async ({page}) => {
+        const onLoginPage = new LoginPage(page)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.verifySignInButtonIsDisabled(false)
+        await onLoginPage.clearField('Email')
+        await onLoginPage.verifySignInButtonIsDisabled(true)
+        await onLoginPage.enterConfiguredValidEmail()
+        await onLoginPage.verifySignInButtonIsDisabled(false)
+        await onLoginPage.clearField('Password')
+        await onLoginPage.verifySignInButtonIsDisabled(true)
+        await onLoginPage.enterConfiguredValidPassword()
+        await onLoginPage.verifySignInButtonIsDisabled(false)
+    })
+
+});
